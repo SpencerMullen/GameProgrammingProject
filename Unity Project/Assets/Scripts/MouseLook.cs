2797fe056b04987b982c0ef9d62070ea8ba8c709
spencermullen <spencermullen0@gmail.com>
2022-03-28 18:11:12 -0400

    Fixed movement

---
+++ Unity Project/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+    float pitch = 0;
+    float yaw = 0;
+
+    void Awake(){
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float moveY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        //transform.Rotate(Vector3.up * moveX);
+
+        yaw += moveX;
+        pitch -= moveY;
+        pitch = Mathf.Clamp(pitch, -75, 75);
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, 0);
+
+    }
+}
