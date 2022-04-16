2a9b5b0495fe4a545c3a173539f3946869489f93
yuyang0609 <71703309+yuyang0609@users.noreply.github.com>
2022-04-15 21:08:05 -0400

    updated

--- Unity Project/Assets/Scripts/Timer.cs
+++ Unity Project/Assets/Scripts/Timer.cs
@@ -2,49 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
-using UnityEngine.UI;
-using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Timer : MonoBehaviour
 {
-    int countdown = 120;
-    public Text timerUI;
+    int enemyLeft;
+    public TextMeshProUGUI enemyText;
     GameObject[] enemies;
-    public AudioClip win;
-    public AudioClip lose;
     // Start is called before the first frame update
     void Start()
     {
-        countDownTimer();
-        
+
         
     }
 
-    // Update is called once per frame
-    void countDownTimer() {  
-    if (countdown > 0 ) {  
-      TimeSpan spanTime = TimeSpan.FromSeconds(countdown);  
-      timerUI.text = "Timer : " + spanTime.Minutes + " : " + spanTime.Seconds;  
-      countdown--;  
-      Invoke("countDownTimer", 1.0f);  
-    } 
-    else {  
-      timerUI.text = "GameOver!";
-      AudioSource.PlayClipAtPoint(lose, Camera.main.transform.position);
-      Invoke("loadGame", 2.0f);  
-    }  
-    }  
+    // Update is called once per frame  
     void Update() {
       enemies = GameObject.FindGameObjectsWithTag("Enemy");
-      if(enemies.Length == 0){
-        timerUI.text = "You Win!";
-        AudioSource.PlayClipAtPoint(win, Camera.main.transform.position);
-        Invoke("loadGame", 1.0f);  
-    }
+      enemyLeft = enemies.Length;
+      enemyText.text = "enemies left: "+ enemyLeft;
   }
-    void loadGame()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-    }  
+     
 }
 
